547ec9b8e1ba5a34ec5d45cabb04253ec0df90b5
LyrinePoliarco <152787755+LyrinePoliarco@users.noreply.github.com>
2025-02-05 01:40:53 +0000

    .

---
+++ activity1.sh
@@ -0,0 +1,14 @@
+USER_NAME="student"
+                 
+echo "Hello, $USER_NAME! welcome to Bash scripting."
+
+echo "Enter your name: "
+read USER_NAME
+
+echo "Enter your age: "
+read USER_AGE
+
+echo "Enter your program: "
+read USER_PROGRAM
+
+echo "Hello, $USER_NAME! your age is $USER_AGE & your program is $USER_PROGRAM"
